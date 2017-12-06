72d7ef32afbd5150e67f2e1f704c49a09d9224f8
my9074 <313231068@qq.com>
2017-12-06 15:35:58 +0800

    build(Build): Add build-docs script and some 3td lib with package.json

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -3,9 +3,44 @@ const path = require('path')
 const utils = require('./utils')
 const config = require('../config')
 const vueLoaderConfig = require('./vue-loader.conf')
+const MarkdownItContainer = require('markdown-it-container')
+const striptags = require('./strip-tags')
 
-function resolve (dir) {
+function resolve(dir) {
   return path.join(__dirname, '..', dir)
+}
+
+const vueMarkdown = {
+  preprocess: (MarkdownIt, source) => {
+    MarkdownIt.renderer.rules.table_open = function () {
+      return '<table class="table">'
+    }
+    MarkdownIt.renderer.rules.fence = utils.wrapCustomClass(MarkdownIt.renderer.rules.fence)
+    const code_inline = MarkdownIt.renderer.rules.code_inline
+    MarkdownIt.renderer.rules.code_inline = function(...args){
+      args[0][args[1]].attrJoin('class', 'code_inline')
+      return code_inline(...args)
+    }
+    return source
+  },
+  use: [
+    [MarkdownItContainer, 'demo', {
+      validate: params => params.trim().match(/^demo\s*(.*)$/),
+      render: function(tokens, idx) {
+        var m = tokens[idx].info.trim().match(/^demo\s*(.*)$/);
+        if (tokens[idx].nesting === 1) {
+          var desc = tokens[idx + 2].content;
+          const html = utils.convertHtml(striptags(tokens[idx + 1].content, 'script'))
+          tokens[idx + 2].children = [];
+
+          return `<demo-block>
+                        <div slot="desc">${html}</div>
+                        <div slot="highlight">`;
+        }
+        return '</div></demo-block>\n';
+      }
+    }]
+  ]
 }
 
 const createLintingRule = () => ({
@@ -36,6 +71,8 @@ module.exports = {
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('examples'),
+      '@packages': resolve('packages'),
+      '@src': resolve('src')
     }
   },
   module: {
@@ -77,7 +114,8 @@ module.exports = {
       },
       {
         test: /\.md$/,
-        loader: 'vue-markdown-loader'
+        loader: 'vue-markdown-loader',
+        options: vueMarkdown
       }
     ]
   },
