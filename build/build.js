7c86c9c47a359137322b3618d567209e42553784
my9074 <313231068@qq.com>
2017-12-12 14:25:02 +0800

    feat(Demand): Added the function to load on demand

--- build/build.js
+++ build/build.js
@@ -11,7 +11,7 @@ const webpack = require('webpack')
 const config = require('../config')
 const webpackConfig = require('./webpack.dist.conf')
 
-const spinner = ora('building for production...')
+const spinner = ora('building for library common...')
 spinner.start()
 
 rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
@@ -32,10 +32,6 @@ rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
       process.exit(1)
     }
 
-    console.log(chalk.cyan('  Build complete.\n'))
-    console.log(chalk.yellow(
-      '  Tip: built files are meant to be served over an HTTP server.\n' +
-      '  Opening index.html over file:// won\'t work.\n'
-    ))
+    console.log(chalk.cyan('  Build common complete.\n'))
   })
 })
---
+++ build/webpack.dist-components.conf.js
@@ -0,0 +1,65 @@
+'use strict'
+const path = require('path')
+const utils = require('./utils')
+const webpack = require('webpack')
+const config = require('../config')
+const Components = require('../components.json')
+var nodeExternals = require('webpack-node-externals');
+const baseWebpackConfig = require('./webpack.base.conf')
+
+function resolve(dir) {
+  return path.join(__dirname, '..', dir)
+}
+
+let env = process.env.NODE_ENV === 'testing' ? require('../config/test.env') : config.dist.env
+
+let rules = baseWebpackConfig.module.rules
+// find vue-loader and disable css source map & extract
+for (let i = 0; i < rules.length; i++) {
+  if (rules[i].loader === 'vue-loader') {
+    rules[i].options = {
+      loaders: utils.cssLoaders({
+        sourceMap: false,
+        extract: false
+      })
+    }
+    break
+  }
+}
+
+// externals
+let externals = {};
+
+Object.keys(Components).forEach(function(key) {
+  externals[`ballon/packages/${key}`] = `ballon/dist/${key}`;
+});
+
+externals = [Object.assign({
+  vue: 'vue'
+}, externals), nodeExternals()];
+
+// alias
+let alias = {
+  '@': resolve('examples'),
+  '@packages': resolve('packages'),
+  '@src': resolve('src')
+};
+
+let webpackConfig = {
+  entry: Components,
+  externals: externals,
+  output: {
+    path: config.dist.assetsRoot,
+    filename: '[name].js',
+    libraryTarget: 'commonjs2',
+  },
+  resolve: {
+    extensions: ['.js', '.vue', '.json'],
+    alias: alias
+  },
+  module: {
+    rules: rules
+  }
+}
+
+module.exports = webpackConfig
